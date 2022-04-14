64280d8e13efe7a70585b946a082e9f2887df964
Valeryi <valerkapushok@gmail.com>
2022-04-14 23:00:18 +0300

    Homework4

---
+++ Homework4/Homework4/Task3.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+
+
+{
+    class Program
+    {
+        /// <summary>Структура, содержащая Login и Password</summary>
+        struct Account
+        {
+            public string Login;
+            public string Password;
+
+            /// <summary>Метод, считывающий логин и пароль из файла</summary>
+            /// <param name="filename">Название файла</param>
+            public static Account[] LoadFromFile(string filename)
+            {
+
+                StreamReader sr = new StreamReader(filename);
+
+                Account[] buf = new Account[20];
+                int counter = 0;
+
+                while (!sr.EndOfStream)
+                {
+
+                    string Login = sr.ReadLine();
+
+                    string Password = sr.ReadLine();
+                    Account account = new Account();
+                    account.Login = Login;
+                    account.Password = Password;
+
+                    buf[counter] = account;
+                    counter++;
+
+                }
+                Account[] arr = new Account[counter];
+
+                Array.Copy(buf, arr, counter);
+
+
+                return arr;
+
+            }
+
+        }
+
+       
+        /// <summary>Метод поверяет корректность логина и пароля</summary>
+        /// <param name="toCheck">Стурктура для проверки</param>
+        /// <returns></returns>
+        static bool CheckLogAndPass(Account toCheck)
+        {
+            if (toCheck.Login == "root" && toCheck.Password == "GeekBrains")
+                return true;
+            else
+                return false;
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует программа проверки логина и пароля, считанных из файла.");
+            
+
+            string fileName = "Logins.txt" ;
+
+            Console.WriteLine("\nЗагрузим файл: " + fileName);
+            Account[] logins = Account.LoadFromFile(fileName);
+
+            for (int i = 0; i < logins.Length; i++)
+
+            {
+               
+
+                Console.Write("Попытка авторизации: ");
+
+                if (CheckLogAndPass(logins[i]))
+                {
+
+                    break;
+                }
+                else
+                {
+                    
+                    Console.WriteLine("Неверный ввод логина или пароля.");
+
+                        
+                }
+                
+            }
+
+            Console.Write("Авторизация успешна!");
+
+            Console.ReadKey();
+        }
+
+
+
+    }
+
+
+}
---
+++ Homework4/Homework4/task2.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Homework4
+
+ //   Реализуйте задачу 1 в виде статического класса StaticClass;
+ // а) Класс должен содержать статический метод, который принимает на вход массив и решает задачу 1;
+ // б) Добавьте статический метод для считывания массива из текстового файла.Метод должен возвращать 
+ //    массив целых чисел;
+ 
+{
+    public static class MyUtils
+    {
+
+        public static int[] LoadFromFile(string fileName)
+        {
+            
+            try
+            {
+                StreamReader streamReader = new StreamReader(fileName);
+                int[] buf = new int[1000];
+                int counter = 0;
+
+                while (!streamReader.EndOfStream)
+                {
+                    buf[counter] = int.Parse(streamReader.ReadLine());
+                    counter++; 
+                }
+
+                int[] arr = new int[counter];
+
+                Array.Copy(buf, arr, counter);
+
+                return arr;
+            }
+            catch
+            {
+                Console.WriteLine("Произошла ошибка чтения файла. Завершение работы приложения.");
+                return null;
+            }
+        }
+
+        public static int DivBy3(string fileName)
+        {
+            int[] am = LoadFromFile(fileName);
+
+            int count = 0;
+            for (int i = 0; i < am.Length - 1; i++)
+            {
+                if (am[i] % 3 == 0 ^ am[i + 1] % 3 == 0)
+                    count++;
+                Console.WriteLine("Пара чисел: {0} и {1}", am[i], am[i + 1]);
+
+            }
+            Console.WriteLine("Количество пар: " + count);
+            Console.ReadLine();
+            return count;
+
+        }
+
+        internal static void PrintArr(object arr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            MyUtils.DivBy3(AppDomain.CurrentDomain.BaseDirectory + "TextFile1.txt");
+
+            Console.ReadLine();
+
+                   }
+    }
+
+    public class Task02
+    {
+        private int[] am;
+
+        public Task02(int n, int min, int max)
+        {
+            am = new int[n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+                am[i] = random.Next(min, max);
+
+        }
+
+        public int[] ReadArrayFromFile(string fileName)
+        {
+            int[] retArr;
+            try
+            {
+                retArr = File.ReadLines(fileName).Select(l => Convert.ToInt32(l)).ToArray();
+            }
+            catch (Exception ex)
+            {
+                retArr = new int[0];
+                Console.WriteLine($"File {fileName} not found. {ex}");
+            }
+            return retArr;
+
+
+        }
+
+
+
+    }
+
+}
+
+
